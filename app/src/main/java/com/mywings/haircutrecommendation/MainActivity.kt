5ac809a25f00ecbf41a433e05884eba40f8655ea
Tatyabhau R. Chavan <tatyabhau.chavan@gmail.com>
2019-01-23 21:15:23 +0530

    Initial commit

---
+++ app/src/main/java/com/mywings/haircutrecommendation/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.mywings.haircutrecommendation
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnSignUp.setOnClickListener {
+            val intent = Intent(this@MainActivity, RegistrationActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
